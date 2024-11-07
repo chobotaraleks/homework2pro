5717c26afb754a942f4fe89cba35e4217920b6d8
chobotaraleks <chobotaraleks@gmail.com>
2024-11-08 01:13:25 +0200

    Added kilometers to miles conversion feature

--- scr/app/Main.java
+++ scr/app/Main.java
@@ -3,5 +3,13 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Converter Project: Converts miles to kilometers and vice versa.");
+
+        // Виклик методу milesToKilometers
+        System.out.println("5 miles is " + milesToKilometers(5) + " kilometers.");
+    }
+
+    // Оголошення методу milesToKilometers в межах класу
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
     }
 }
